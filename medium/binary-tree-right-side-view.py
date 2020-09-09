18d5c66ccdba6f107864b7f7c819de3f392ead7e
aakif-h <54697234+aakif-h@users.noreply.github.com>
2020-09-08 18:22:13 -0700

    Update binary-tree-right-side-view.py
    no need to do a second pass; if an element is already in the result, that's the closest element viewable by the person (for its respective level)

--- medium/binary-tree-right-side-view.py
+++ medium/binary-tree-right-side-view.py
@@ -12,13 +12,9 @@ class Solution:
             while queue:
                 node, level = queue.pop(0)
                 if len(res) <= level:
-                    res.append([node.val])
-                else:
-                    res[level].append(node.val)
+                    res.append(node.val)
                 if node.right:
                     queue.append([node.right, level+1])
                 if node.left:
                     queue.append([node.left, level+1])
-            for i in range(level+1):
-                res[i] = res[i][0]
         return res
